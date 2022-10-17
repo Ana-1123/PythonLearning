0e4e2ab9688e56e2bbab83997951d6e96574c453
Ciobanu Ana <75687295+Ana-1123@users.noreply.github.com>
2022-10-17 14:35:02 +0300

    Modificare pentru cazul:  not same number of items

--- Laborator 2/Lab2Prob10.py
+++ Laborator 2/Lab2Prob10.py
@@ -1,14 +1,20 @@
+
 def listoftuples(*lists):
     tlist = []
     length = max([len(k) for k in lists])
 
     for i in range(0, length):
-        plist = []
-        for list in lists:
-            plist.append(list[i])
-        tlist.append(tuple(plist))
+      plist = []
+      for list in lists:
+            if i >= len(list):
+                plist.append(None)
+            else:
+                plist.append(list[i])
+      tlist.append(tuple(plist))
     return tlist
 
-
 if __name__ == '__main__':
-    print(listoftuples([1, 2, 3], [5, 6, 7], ["a", "b", "c"]))
+    print(listoftuples([1, 2], [5, 6, 7], ["a", "b", "c"]))
+
+
+
