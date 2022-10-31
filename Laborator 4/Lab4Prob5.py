75e040836a95e75c6c216b04e59581834b9084ac
Ciobanu Ana <75687295+Ana-1123@users.noreply.github.com>
2022-10-31 14:29:04 +0200

    Modificare open file

--- Laborator 4/Lab4Prob5.py
+++ Laborator 4/Lab4Prob5.py
@@ -5,17 +5,23 @@ def fileordirectory(target, to_search):
     listoffiles = []
     try:
         if os.path.isfile(target):
-            openfile = open(target, "rt").read()
+            # openfile = open(target, "rt").read()
+            f1 = open(target, "rt")
+            openfile = f1.read()
             if openfile.count(to_search) > 0:
                 listoffiles.append(target)
+            f.close()
             return listoffiles
         elif os.path.isdir(target):
             for root, dirs, files in os.walk(target):
                 for file in files:
                     f = os.path.join(target, file)
-                    openfile = open(f, 'rt').read()
+                    # openfile = open(f, 'rt').read()
+                    f1 = open(f, "rt")
+                    openfile = f1.read()
                     if openfile.count(to_search) > 0:
                         listoffiles.append(file)
+            f.close()
             return listoffiles
         else:
             raise ValueError("Stringul dat nu contine o cale nici de fisier nici de director")
--- Laborator 4/Lab4Prob6.py
+++ Laborator 4/Lab4Prob6.py
@@ -9,17 +9,23 @@ def fileordirectory2(target, to_search, callback):
     listoffiles = []
     try:
         if os.path.isfile(target):
-            openfile = open(target, "rt").read()
+            # openfile = open(target, "rt").read()
+            f1 = open(target, "rt")
+            openfile = f1.read()
             if openfile.count(to_search) > 0:
                 listoffiles.append(target)
+            f1.close()
             return listoffiles
         elif os.path.isdir(target):
             for root, dirs, files in os.walk(target):
                 for file in files:
                     f = os.path.join(target, file)
-                    openfile = open(f, 'rt').read()
+                    # openfile = open(f, 'rt').read()
+                    f1 = open(f, "rt")
+                    openfile = f1.read()
                     if openfile.count(to_search) > 0:
                         listoffiles.append(file)
+            f1.close()
             return listoffiles
         else:
             raise ValueError("Stringul dat nu contine o cale nici de fisier nici de director")
